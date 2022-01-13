1c837f92d526ef52db63a974a5bbb28d841d0bbc
peling <40432059+pelingundogdu@users.noreply.github.com>
2022-01-13 10:55:08 +0100

    upload, 9.2 deepexplain analysis for each circuit node

---
+++ notebooks/9.2-pg-deepExplain_detail.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+
+import os, sys
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath('__file__')))
+os.chdir(ROOT_DIR)
+sys.path.append(ROOT_DIR)
+
+# from scripts import config as src
+import pandas as pd
+import numpy as np
+
+import seaborn as sns
+import datetime as dt
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import OneHotEncoder
+import tensorflow as tf
+import tensorflow.compat.v1 as tf1
+tf1.disable_eager_execution()
+
+# Import DeepExplain
+from deepexplain.tensorflow import DeepExplain
+import warnings
+warnings.filterwarnings('ignore')
+
+import os
+# os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+# os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+rand_state = 91
+dataset              = 'immune_new.pck'
+experiment           = 'exper_immune'
+analysis             = 'activation'
+split                = 'train_test_split_tuningFalse_filtergeneFalse'
+model_detail         = 'circuits_1_layer'
+bio_knowledge        = 'pbk_circuit_hsa_sig.txt'
+# model_detail         = 'pathways_1_layer'
+# bio_knowledge        = 'pbk_layer_hsa_sig.txt'
+
+df = pd.read_pickle(os.path.join('./data/processed/', experiment, dataset))
+# df = df.sample(n=20, replace=True, random_state=1) # sampling
+ohe = OneHotEncoder()
+X_data = df.iloc[:, :-1].values
+y_data = df.iloc[:, -1:].values
+y_ohe = ohe.fit_transform(y_data).toarray()
+print(X_data.shape)
+print(y_data.shape)
+
+print(df.groupby('cell_type').size())
+
+df_pbk = pd.read_csv(f'./data/processed/{bio_knowledge}').set_index('symbol')
+
+df_pbk_final = pd.merge(left=pd.DataFrame(df.columns[:-1]).set_index(0)
+                        , right=df_pbk
+                        , left_index=True
+                        , right_index=True
+                        , how='left').fillna(0)
+# df_pbk_final = df_pbk_final.iloc[:, :10]
+df_pbk_final
+
+        
+for experiment_index in range(0,100):
+    sess = tf.compat.v1.Session()
+    graph = tf.compat.v1.get_default_graph()
+    init = tf.compat.v1.global_variables_initializer()
+    sess.run(init)
+
+    print(f'iteration no, {experiment_index}')
+    model_name =f'design_{model_detail}_immune_new_Adam_relu_{experiment_index}.h5'
+    with sess.graph.as_default():
+
+#         tf.compat.v1.keras.backend.set_session(sess)
+        tf1.keras.backend.set_session(sess)
+
+        model = tf.compat.v1.keras.models.load_model(filepath=f'./models/{experiment}/{split}/{model_name}')
+
+        model.summary()
+        # predictions = model.predict(X_data)
+        time_start = dt.datetime.now().time().strftime('%H:%M:%S') 
+        print(f'start time, {time_start}')
+        
+        circuit_gene_index = dict()
+        method_name = 'elrp'
+
+        with DeepExplain(session=tf1.keras.backend.get_session() ) as de:
+        #     defining the input size which is the number of gene in given dataset, input_tensor--> X
+            X = model.layers[0].input
+        #     the target layer size which shows the biological layer, target_tensor --> T
+            T = model.layers[1].input
+        #     the mask for the filtering the target node in for analysis
+            mask_ys = np.zeros((1, T.shape[1]))
+            xs = X_data
+            print(f'input_tensor, {X}\ntarget_tensor, {T}\nmask_ys, {mask_ys.shape}\nxs, {xs.shape}')
+
+        #     iterating the filtering mask for each node in the biological layer which defined as T
+            for i_circuit in range(T.shape[1]):
+                if i_circuit % 500 == 0:
+                    print(i_circuit)
+        #         filtering the target node in target layer with assigning value as 1
+                mask_ys[:, i_circuit] = 1.0
+        #         compute attributions of each genes ('X') for each sample defined as 'xs' with defined mask 'T * mask_ys'
+                explain = de.explain(method_name, T * mask_ys, X, xs)
+        #         concat cell type with explain matrix
+                df_explain = pd.concat([pd.DataFrame(explain, columns=df.columns[:-1]), pd.DataFrame(y_data, columns=['cell_type']) ], axis=1)
+                df_explain.to_csv(f'./reports/deepexplain/sample_details_new/{experiment}/{model_detail}_{method_name}_{experiment_index}_{df_pbk_final.columns[i_circuit]}.csv.gzip'
+                                 , compression='gzip')
+
+        time_end = dt.datetime.now().time().strftime('%H:%M:%S') 
+        print(f'end time, {time_end}')
+        
