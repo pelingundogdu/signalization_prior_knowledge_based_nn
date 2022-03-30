ff4bdf8c7917b106890b1b3a39d6bfb34e5f7d73
peling <40432059+pelingundogdu@users.noreply.github.com>
2022-03-30 11:43:32 +0200

    add, project bash script

--- project_bash.sh
+++ project_bash.sh
@@ -1,50 +1,34 @@
-# ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath('__file__')))
-# os.chdir(ROOT_DIR)
-# sys.path.append(ROOT_DIR)
+# hsa --> homo sapiens(human), org.Hs.eg.db
+#  Genome Wide annotation
+# https://bioconductor.org/packages/3.12/data/annotation/
 
-# os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-
-### hsa --> homo sapiens(human), org.Hs.eg.db
-### mmu --> mus musculus(mouse), org.Mm.eg.db
-
-###  Genome Wide annotation
-### https://bioconductor.org/packages/3.12/data/annotation/
-
-### StandardScaler\(\)
-### FunctionTransformer\(np.log1p\)
-
-# #!/bin/bash
-
-date
-
+############################## 1.EXPORTING PRIOR BIOLOGICAL KNOWLEDGE LAYER (START) ###############################
+# Exporting pathway info
 echo "Exporting signaling pathway information from hipathia --- scripts/pathway_layer_data/1.1-pg-pathway-from-hipathia.r executing..."
-# Rscript scripts/pathway_layer_data/1.1-pg-pathway-from-hipathia.r -sp hsa -src hipathia
-# Rscript scripts/pathway_layer_data/1.1-pg-pathway-from-hipathia.r -sp mmu -src hipathia
-
+Rscript scripts/pathway_layer_data/1.1-pg-pathway-from-hipathia.r -sp hsa -src hipathia
+# Remowing disease related pathways
 echo "Processing the pathway list, removing disease related pathways --- scripts/pathway_layer_data/1.2-pg-remove-disease-cancer.py executing..."
-# python scripts/pathway_layer_data/1.2-pg-remove-disease-cancer.py -sp hsa -src hipathia
-# python scripts/pathway_layer_data/1.2-pg-remove-disease-cancer.py -sp mmu -src hipathia
-
+python scripts/pathway_layer_data/1.2-pg-remove-disease-cancer.py -sp hsa -src hipathia
+# exporting gene list
 echo "Exporting gene list based on processed pathway list in 1.2-pg-remove-disease-cancer.py --> scripts/pathway_layer_data/1.3-pg-gene-from-hipathia.r executing..."
-# Rscript scripts/pathway_layer_data/1.3-pg-gene-from-hipathia.r -sp hsa -src hipathia
-# Rscript scripts/pathway_layer_data/1.3-pg-gene-from-hipathia.r -sp mmu -src hipathia
-
-echo "Converting entrex id value into gene symbol -->  scripts/pathway_layer_data/1.4-pg-gene-id-entrez-converter.r executed!!"
-# Rscript scripts/pathway_layer_data/1.4-pg-gene-id-entrez-converter.r -sp hsa -src hipathia -ga org.Hs.eg.db
-# Rscript scripts/pathway_layer_data/1.4-pg-gene-id-entrez-converter.r -sp mmu -src hipathia -ga org.Mm.eg.db
-
+Rscript scripts/pathway_layer_data/1.3-pg-gene-from-hipathia.r -sp hsa -src hipathia
+# Converting entrez id into gene symbol
+echo "Converting entrez id value into gene symbol -->  scripts/pathway_layer_data/1.4-pg-gene-id-entrez-converter.r executed!!"
+Rscript scripts/pathway_layer_data/1.4-pg-gene-id-entrez-converter.r -sp hsa -src hipathia -ga org.Hs.eg.db
+# Creating pathway-gene matrix
 echo "Creating prior biological knowledge information to include the nn design in first hidden layer --> scripts/pathway_layer_data/1.5-pg-creating-biological-layer.py executing..."
-# python scripts/pathway_layer_data/1.5-pg-creating-biological-layer.py -sp hsa -src hipathia
-# python scripts/pathway_layer_data/1.5-pg-creating-biological-layer.py -sp mmu -src hipathia
-
+python scripts/pathway_layer_data/1.5-pg-creating-biological-layer.py -sp hsa -src hipathia
 echo "PATHWAY INFORMATION EXPORTED!!!"
-
 echo "Exporting data/processed/pbk_layer_{BIO_SOURCE} files"
-# python scripts/bio_layer_data/1.0-pg-exporting-bio-layer.py
+python scripts/bio_layer_data/1.0-pg-exporting-bio-layer.py
 
+# Creating circuit matrix
+echo "Exporting circuits matrix --> scripts/pathway_layer_data/1.6-pg-creating-biological-layer_circuits.py"
+python scripts/pathway_layer_data/1.6-pg-creating-biological-layer_circuits.py
+############################### 1.EXPORTING PRIOR BIOLOGICAL KNOWLEDGE LAYER (END) ################################
 
+######################################### 2.DATASET PREPROCESSING (START) #########################################
 echo "PREPROCESSING EXPERIMENTS' DATASETS"
-
 # $ python notebooks/2.0-pg-preprocessing-dataset.py -exp {EXPERIMENT NAME}
 #                                                    -ds  {DATASET NAME} 
 #                                                    -sc  {SCALER, StandardScaler(ss), MinMaxScaker(mms), Log1p} 
@@ -52,242 +36,148 @@ echo "PREPROCESSING EXPERIMENTS' DATASETS"
 #                                                    -ofn {OUTPUT FILE NAME}
 
 echo "PREPROCESSING of EXPERIMENT MELANOMA DATASET"
-
 python notebooks/2.0-pg-preprocessing-dataset.py \
     -exp exper_melanoma \
     -ds reference.pck \
     -sw False \
     -sc log1p \
     -tci -1 \
-    -ofn reference &&
+    -ofn reference_log1p &&
 python notebooks/2.0-pg-preprocessing-dataset.py \
     -exp exper_melanoma \
     -ds query.pck \
     -sw False \
     -sc log1p \
     -tci -1 \
-    -ofn query
-    
-python notebooks/2.0-pg-preprocessing-dataset.py \
-    -exp exper_melanoma \
-    -ds query_reference_wo_negcell.pck \
-    -sw False \
-    -sc log1p \
-    -tci -1 \
-    -ofn query_reference_wo_negcell
-
-echo "PREPROCESSING of EXPERIMENT MOUSE DATASET"
-
-python notebooks/2.0-pg-preprocessing-dataset.py \
-    -exp exper_mouse \
-    -ds 1-3_integrated_NNtraining.pck \
-    -sw False \
-    -sc ss \
-    -tci 0 \
-    -ofn mouse_learning &&
-python notebooks/2.0-pg-preprocessing-dataset.py \
-    -exp exper_mouse \
-    -ds 3-33_integrated_retrieval_set.pck \
-    -sw False \
-    -sc ss \
-    -tci 0 \
-    -ofn mouse_retrieval
-    
-python notebooks/2.0-pg-preprocessing-dataset.py \
-    -exp exper_mouse \
-    -ds 3-33_integrated_retrieval_set_cv.pck \
-    -sw False \
-    -sc ss \
-    -tci -1 \
-    -ofn mouse_retrieval_cv
+    -ofn query_log1p
 
 echo "PREPROCESSING of EXPERIMENT PBMC DATASET"
-######## MAGIC ########
-## magic with sample-wise and log normalization
-# python notebooks/3.1-pg-preprocessing-experiment-dataset.py -exp exper_pbmc -loc processed -ds Immune_magic_sw.pck -pbk pbk_layer_hsa.txt -sc FunctionTransformer\(np.log1p\) -tci -1
-
-######### RAW #########
-
 python notebooks/2.0-pg-preprocessing-dataset.py \
     -exp exper_pbmc \
     -ds Immune.pck \
     -sw True \
     -sc log1p \
     -tci -1 \
-    -ofn pbmc_fig2
+    -ofn pbmc_sw_log1p
 
 echo "PREPROCESSING of EXPERIMENT IMMUNE DATASET"
-
 python notebooks/2.0-pg-preprocessing-dataset.py \
     -exp exper_immune \
     -ds Fig3g.pck \
     -sw False \
     -sc None \
     -tci -1 \
-    -ofn immune_fig3
+    -ofn immune_new
+########################################## 2.DATASET PREPROCESSING (END) ##########################################
 
-
+############################################### 3.EXPERIMENT (START) ##############################################
 echo "NEURAL NETWORK TRAINING"
+# network parameters
+optimizer_var='Adam'
+activation_var='relu'
+tuning='False'
+filter_space='False'
+# prior biological knowledge detail
+pbk_var='pbk_circuit_hsa_sig.txt' #circuits
+pbk_info='circuits'
 
-############################################## MELANOMA EXPERIMENT ##############################################
-echo "MODEL TRAINING for MELANOMA EXPERIMENT"
-
-# 1-LAYER DENSE100
-python notebooks/4.0-pg-model.py \
-    -design 1_layer_dense100 \
-    -first_hidden_layer_pbk None \
-    -first_hidden_layer_dense 100 \
-    -second_hidden_layer False \
-    -optimizer Adam \
-    -activation relu \
-    -ds processed/exper_melanoma/reference_log1p.pck \
-    -split StratifiedKFold \
-    -filter_gene_space False &&
-python notebooks/4.0-pg-model.py \
-    -design 1_layer_dense100 \
-    -first_hidden_layer_pbk None \
-    -first_hidden_layer_dense 100 \
-    -second_hidden_layer False \
-    -optimizer Adam \
-    -activation relu \
-    -ds processed/exper_melanoma/reference_log1p.pck \
-    -split train_test_split \
-    -filter_gene_space False &&
-python notebooks/4.0-pg-model.py \
-    -design 1_layer_dense100 \
-    -first_hidden_layer_pbk None \
-    -first_hidden_layer_dense 100 \
-    -second_hidden_layer False \
-    -optimizer Adam \
-    -activation relu \
-    -ds processed/exper_melanoma/reference_log1p.pck \
-    -split LeaveOneGroupOut \
-    -filter_gene_space False
+########################################### a.IMMUNE EXPERIMENT (START) ###########################################
+echo "IMMUNE EXPERIMENT"
+# analysis_var='encoding'
+# analysis_var='performance'
+analysis_var='evaluate_rskf'
+ds_var='processed/exper_immune/immune_new.pck'
 
 # 1-LAYER SIGNALING
 python notebooks/4.0-pg-model.py \
-    -design 1_layer_signaling \
-    -first_hidden_layer_pbk pbk_layer_hsa_sig.txt \
+    -design ${pbk_info}_1_layer \
+    -first_hidden_layer_pbk $pbk_var \
     -first_hidden_layer_dense 0 \
     -second_hidden_layer False \
-    -optimizer Adam \
-    -activation relu \
-    -ds processed/exper_melanoma/reference_log1p.pck \
-    -split StratifiedKFold \
-    -filter_gene_space False &&
-python notebooks/4.0-pg-model.py \
-    -design 1_layer_signaling \
-    -first_hidden_layer_pbk pbk_layer_hsa_sig.txt \
-    -first_hidden_layer_dense 0 \
-    -second_hidden_layer False \
-    -optimizer Adam \
-    -activation relu \
-    -ds processed/exper_melanoma/reference_log1p.pck \
-    -split train_test_split \
-    -filter_gene_space False &&
-python notebooks/4.0-pg-model.py \
-    -design 1_layer_signaling \
-    -first_hidden_layer_pbk pbk_layer_hsa_sig.txt \
-    -first_hidden_layer_dense 0 \
-    -second_hidden_layer False \
-    -optimizer Adam \
-    -activation relu \
-    -ds processed/exper_melanoma/reference_log1p.pck \
-    -split LeaveOneGroupOut \
-    -filter_gene_space False
+    -optimizer $optimizer_var \
+    -activation $activation_var \
+    -ds $ds_var \
+    -analysis $analysis_var \
+    -filter_gene_space $filter_space \
+    -hp_tuning $tuning 
     
 # 2-LAYER SIGNALING
 python notebooks/4.0-pg-model.py \
-    -design 2_layer_signaling \
-    -first_hidden_layer_pbk pbk_layer_hsa_sig.txt \
+    -design ${pbk_info}_2_layer \
+    -first_hidden_layer_pbk $pbk_var \
     -first_hidden_layer_dense 0 \
     -second_hidden_layer True \
-    -optimizer Adam \
-    -activation relu \
-    -ds processed/exper_melanoma/reference_log1p.pck \
-    -split StratifiedKFold \
-    -filter_gene_space False &&
+    -optimizer $optimizer_var \
+    -activation $activation_var \
+    -ds $ds_var \
+    -analysis $analysis_var \
+    -filter_gene_space $filter_space \
+    -hp_tuning $tuning
+############################################ a.IMMUNE EXPERIMENT (END) ############################################
+
+############################################ b.PBMC EXPERIMENT (START) ############################################
+echo "PBMC EXPERIMENT"
+analysis_var='evaluate_rskf'
+ds_var='processed/exper_pbmc/pbmc_sw_log1p.pck'
+
+# 1-LAYER SIGNALING
 python notebooks/4.0-pg-model.py \
-    -design 2_layer_signaling \
-    -first_hidden_layer_pbk pbk_layer_hsa_sig.txt \
-    -first_hidden_layer_dense 0 \
-    -second_hidden_layer True \
-    -optimizer Adam \
-    -activation relu \
-    -ds processed/exper_melanoma/reference_log1p.pck \
-    -split train_test_split \
-    -filter_gene_space False &&
-python notebooks/4.0-pg-model.py \
-    -design 2_layer_signaling \
-    -first_hidden_layer_pbk pbk_layer_hsa_sig.txt \
-    -first_hidden_layer_dense 0 \
-    -second_hidden_layer True \
-    -optimizer Adam \
-    -activation relu \
-    -ds processed/exper_melanoma/reference_log1p.pck \
-    -split LeaveOneGroupOut \
-    -filter_gene_space False 
-    
-## CROSS-VALIDATION
-python notebooks/4.0-pg-model.py \
-    -design 1_layer_signaling \
-    -first_hidden_layer_pbk pbk_layer_hsa_sig.txt \
+    -design ${pbk_info}_1_layer \
+    -first_hidden_layer_pbk $pbk_var \
     -first_hidden_layer_dense 0 \
     -second_hidden_layer False \
-    -optimizer Adam \
-    -activation relu \
-    -ds processed/exper_melanoma/reference_log1p.pck \
-    -split RepeatedStratifiedKFold \
-    -filter_gene_space False    
+    -optimizer $optimizer_var \
+    -activation $activation_var \
+    -ds $ds_var \
+    -analysis $analysis_var \
+    -filter_gene_space $filter_space \
+    -hp_tuning $tuning 
+    
+# 2-LAYER SIGNALING
 python notebooks/4.0-pg-model.py \
-    -design 2_layer_signaling \
-    -first_hidden_layer_pbk pbk_layer_hsa_sig.txt \
+    -design ${pbk_info}_2_layer \
+    -first_hidden_layer_pbk $pbk_var \
     -first_hidden_layer_dense 0 \
     -second_hidden_layer True \
-    -optimizer Adam \
-    -activation relu \
-    -ds processed/exper_melanoma/reference_log1p.pck \
-    -split RepeatedStratifiedKFold \
-    -filter_gene_space False
-    
-    
-############################################## MELANOMA EXPERIMENT ##############################################
+    -optimizer $optimizer_var \
+    -activation $activation_var \
+    -ds $ds_var \
+    -analysis $analysis_var \
+    -filter_gene_space $filter_space \
+    -hp_tuning $tuning
+############################################# b.PBMC EXPERIMENT (END) #############################################
 
+######################################### 3c.MELANOMA EXPERIMENT (START) ##########################################
+echo "MELANOMA EXPERIMENT"
+# analysis_var='None'
+analysis_var='encoding'
+ds_var='processed/exper_melanoma/reference_log1p.pck'
 
-
-
-############################################### MOUSE EXPERIMENT ###############################################
-
-echo "NEURAL NETWORK TRAINING for PBMC EXPERIMENT"
-### python notebooks/4.0-pg-model-training.py -exp exper_pbmc -loc processed -ds Immune_magic_sw_log1p.pck -pbk pbk_layer_hsa.txt -split KFold -nncv NN
-# python notebooks/4.0-pg-model-training.py -exp exper_pbmc -loc processed -ds Immune_sw_log1p.pck -pbk pbk_layer_hsa.txt -split KFold -nncv NN
-
-echo "NEURAL NETWORK TRAINING for IMMUNE EXPERIMENT"
+# 1-LAYER SIGNALING
 python notebooks/4.0-pg-model.py \
-    -design 1_layer_signaling \
-    -first_hidden_layer_pbk pbk_layer_hsa_sig.txt \
+    -design ${pbk_info}_1_layer \
+    -first_hidden_layer_pbk $pbk_var \
     -first_hidden_layer_dense 0 \
     -second_hidden_layer False \
-    -optimizer Adam \
-    -activation relu \
-    -ds processed/exper_immune/immune_fig3.pck \
-    -split train_test_split \
-    -filter_gene_space False
+    -optimizer $optimizer_var \
+    -activation $activation_var \
+    -ds $ds_var \
+    -analysis $analysis_var \
+    -filter_gene_space $filter_space \
+    -hp_tuning $tuning
 
+# 2-LAYER SIGNALING
+python notebooks/4.0-pg-model.py \
+    -design ${pbk_info}_2_layer \
+    -first_hidden_layer_pbk $pbk_var \
+    -first_hidden_layer_dense 0 \
+    -second_hidden_layer True \
+    -optimizer $optimizer_var \
+    -activation $activation_var \
+    -ds $ds_var \
+    -analysis $analysis_var \
+    -filter_gene_space $filter_space \
+    -hp_tuning $tuning
+########################################## 3c.MELANOMA EXPERIMENT (END) ###########################################
 
-echo "CROSS VALIDATION"
-echo "CROSS VALIDATION for HUMAN EXPERIMENT"
-# python notebooks/5.0-pg-cross-validation.py -exp exper_melanoma -loc processed -ds reference_query_log1p.pck -pbk pbk_layer_hsa.txt -nncv CV
-
-echo "CROSS VALIDATION for PBMC EXPERIMENT"
-# python notebooks/5.0-pg-cross-validation.py -exp exper_pbmc -loc processed -ds Immune_sw_log1p.pck -pbk pbk_layer_hsa.txt -nncv CV
-
-
-echo "RETRIEVAL ANALYSIS for MOUSE EXPERIMENT"
-
-
-python notebooks/8.0-paper-retrieval.py models/exper_mouse/None 0 saved_model 0 all
-
-python notebooks/8.1-paper-getting-retrieval-summary.py reports/retrieval/exper_mouse/
-
-date
+################################################ 3.EXPERIMENT (END) ###############################################
